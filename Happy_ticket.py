d03ffebea83c1e64b6a66c1af1fd249d1dcabc7d
katya <ekaterinasavina@192.168.1.7>
2024-02-13 21:15:23 +0300

    v2

--- Happy_ticket.py
+++ Happy_ticket.py
@@ -1,23 +1,27 @@
 def is_happy_ticket(ticket):
     count_symbol = len(ticket)
-    middle = int(count_symbol / 2)
-    first_part = ticket[0:middle]
-    second_part = ticket[middle:]
-    index = 0
-    sum_first_part = 0
-    while index < middle:
-        symbol = int((str(first_part)[index]))
-        sum_first_part = sum_first_part + symbol
-        index = index + 1
-    index = 0
-    sum_second_part = 0
-    while index < middle:
-        symbol = int(str(second_part)[index])
-        sum_second_part = int(sum_second_part) + symbol
-        index = index + 1
-    if (sum_first_part == sum_second_part):
-        return('true')
+    if count_symbol % 2 != 0:
+        print('Введите четное число!')
     else:
-        return('false')
+        middle = int(count_symbol / 2)
+        first_part = ticket[0:middle]
+        second_part = ticket[middle:]
+        index = 0
+        sum_first_part = 0
+        while index < middle:
+            symbol = int((str(first_part)[index]))
+            sum_first_part = sum_first_part + symbol
+            index = index + 1
+        index = 0
+        sum_second_part = 0
+        while index < middle:
+            symbol = int(str(second_part)[index])
+            sum_second_part = int(sum_second_part) + symbol
+            index = index + 1
+        if (sum_first_part != sum_second_part):
+            return('false')
+        else:
+            return('true')
 
-print(is_happy_ticket('1111001121'))
+t = input('Введите билет ')
+print(is_happy_ticket(t))
